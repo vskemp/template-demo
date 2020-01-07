4ef4d7d03d85d2a321250b762384e9b89b653bfc
Veronica Kemp <veronicakemp@Veronicas-MacBook-Pro.local>
2020-01-07 14:49:24 -0500

    templates and partials used

--- index.js
+++ index.js
@@ -10,6 +10,58 @@ app.engine('html', es6Renderer);
 app.set('views', 'templates');
 app.set('view engine', 'html');
 
+const blogData = [
+    {
+        title: "first blog post",
+        content: "STUF STUF STUF STUF"
+    },
+    {
+        title: "second blog post",
+        content: "STUF STUF STUF STUF"
+    },
+    {
+        title: "third blog post",
+        content: "STUF STUF STUF STUF"
+    }
+];
+
+app.get("/", (req, res) => {
+    res.render("home", {
+        locals: {
+            pageTitle: "Home Page!",
+            pageHeader: "It's the Home Page!"
+        },
+        partials: {
+            header: "partials/header",
+            nav: "partials/nav",
+            footer: "partials/footer"
+        }
+    });
+});
+
+app.get("/blog", (req, res) => {
+
+    // const blogHTML = [];
+    // for (let post of blogData) {
+    //     blogHTML.push(`<h2>${post.title}</h2>`);
+    //     blogHTML.push(`<p>${post.content}</p>`)
+    // }
+    // const blogHTML = blogData.map((post) => {
+    //     return `<h2>${post.title}</h2><p>${post.content}</p>`});
+    
+    res.render("blog", {
+        locals: {
+            pageTitle: "The Blog",
+            // blogPosts: blogHTML.join(" ")
+            blogPosts: blogData
+        },
+        partials: {
+            header: "partials/header",
+            nav: "partials/nav",
+            footer: "partials/footer"
+        }
+    });
+})
 
 server.listen(PORT, () => {
     console.log(`Listening at ${PORT}`);
